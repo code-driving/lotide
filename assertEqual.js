06c63f131d980dcb2f87ec184bf6e112ed2e91d2
code-driving <tittan7@gmail.com>
2021-03-01 21:20:40 +0000

    Implementation of the assertion function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertiion failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("you", "me");
+assertEqual(5, 5);
+assertEqual("me", "me");
