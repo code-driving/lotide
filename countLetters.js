35e1e9cd75d900381030e560fe1d2ff65357a3e1
code-driving <tittan7@gmail.com>
2021-03-03 20:27:24 +0000

    Adding countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+//use the replace method for the first time to remove spaces :) just to see how it works
+//create an empty object
+//loop through the string
+//check if the object contains a property which corresponds to the character in the string.
+//if not ==> create such property and add one to it as a value (first occurrence)
+//if yes ==> increment the value by one
+const countLetters = function(sentence) {
+  const objOfLetters = {};
+  for (let char of sentence.replace(/[^\w]/g, "")) {
+    if (!Object.keys(objOfLetters).includes(char)) {
+      objOfLetters[char] = 1;
+    } else {
+      objOfLetters[char]++;
+    }
+  }
+  return objOfLetters;
+};
+console.log(countLetters('LHL'));
+console.log(countLetters('Lighthouse Labs is awesome'));
