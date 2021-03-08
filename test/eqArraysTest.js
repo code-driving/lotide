b955943e1e22e1bd48b47f5beb0a1d98f9b2ffc6
code-driving <tittan7@gmail.com>
2021-03-08 19:03:48 +0000

    Modifying eqArrays.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,6 @@
+const { assertEqual, eqArrays } = require("../eqArrays");
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays([[1, 2], 3], [[1, 2], 3]), true);
