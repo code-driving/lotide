2ce51844fc3886a7b09adb178b81310d845fd32d
code-driving <tittan7@gmail.com>
2021-03-09 05:43:00 +0000

    modifying for testing with Mocha, Chai

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,28 +1,16 @@
-// const assertEqual = require("./assertEqual")
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(obj, val) {
   const entries = Object.entries(obj);
   for (const [key, value] of entries) {
     if (value === val) {
       return key;
-    }
+    } 
   }
 };
-
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire",
 };
+console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"));  
 
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
