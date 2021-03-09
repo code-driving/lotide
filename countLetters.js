aab4220ff0eda3666b925e2765390e0b5bb3d293
code-driving <tittan7@gmail.com>
2021-03-09 04:17:35 +0000

    Refactoring with Mocha, Chai

--- countLetters.js
+++ countLetters.js
@@ -1,19 +1,3 @@
-// const assertEqual = require("./assertEqual");
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-//use the replace method for the first time to remove spaces :) just to see how it works
-//create an empty object
-//loop through the string
-//check if the object contains a property which corresponds to the character in the string.
-//if not ==> create such property and add one to it as a value (first occurrence)
-//if yes ==> increment the value by one
 const countLetters = function(sentence) {
   const objOfLetters = {};
   for (let char of sentence.replace(/[^\w]/g, "")) {
@@ -25,7 +9,5 @@ const countLetters = function(sentence) {
   }
   return objOfLetters;
 };
-// console.log(countLetters('LHL'));
-// console.log(countLetters('Lighthouse Labs is awesome'));
 
-
+module.exports = countLetters;
