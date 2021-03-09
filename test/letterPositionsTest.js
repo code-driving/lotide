72e3f65b8d8d97b221ca05b126dd64f039706960
code-driving <tittan7@gmail.com>
2021-03-09 06:06:57 +0000

    testing with Mocha, Chai

---
+++ test/letterPositionsTest.js
@@ -0,0 +1,12 @@
+const assert = require("chai").assert;
+const letterPositions = require("../letterPositions");
+
+describe("#letterPositions", () => {
+  it("returns [1] for ('hello').e", () => {
+    assert.deepEqual(letterPositions("hello").e, [1]);
+  });
+
+  it("returns [0] for ('hello').e", () => {
+    assert.deepEqual(letterPositions("smile").s, [0]);
+  });
+});
