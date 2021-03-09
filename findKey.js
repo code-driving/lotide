481dfd496c319a83d011c51a2a3dd276362287c7
code-driving <tittan7@gmail.com>
2021-03-09 05:23:40 +0000

    removing custom assertions and testing with Mocha, Chai

--- findKey.js
+++ findKey.js
@@ -1,14 +1,4 @@
-// const assertEqual = ("./assertEqual.js")
-
-// const assertEqual = function (actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-//   } else if (actual !== expected) {
-//     console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-//   }
-// };
-
-const findKey = function (obj, callback) {
+const findKey = function(obj, callback) {
   for (const [key, value] of Object.entries(obj)) {
     if (callback(value)) {
       return key;
@@ -16,24 +6,7 @@ const findKey = function (obj, callback) {
   }
 };
 
-console.log(
-  findKey(
-    {
-      "Blue Hill": { stars: 1 },
-      Akaleri: { stars: 3 },
-      noma: { stars: 2 },
-      elBulli: { stars: 3 },
-      Ora: { stars: 2 },
-      Akelarre: { stars: 3 },
-    },
-    (x) => x.stars === 2
-  )
-);
-// assertEqual({
-//   "Blue Hill": { stars: 1 },
-//   Akaleri: { stars: 3 },
-//   noma: { stars: 2 },
-//   elBulli: { stars: 3 },
-//   Ora: { stars: 2 },
-//   Akelarre: { stars: 3 },
-// }, noma)
+module.exports = findKey;
+
+
+
