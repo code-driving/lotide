973335d39e9139f3350b7a38f636c28b4cc5eb7e
code-driving <tittan7@gmail.com>
2021-03-09 05:48:22 +0000

    modifying for testing

--- flatten.js
+++ flatten.js
@@ -1,30 +1,3 @@
-// const eqArray = require("./eqArrays");
-// const assertArraysEqual = require("./assertArraysEqual");
-
-const eqArrays = function(arr1, arr2) {
-  if (!Array.isArray(arr1) && !Array.isArray(arr2)) {
-    return false;
-  }
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let char of arr1) {
-    if (arr1[char] !== arr2[char]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2 = []) {
-  const eqArray = eqArrays(arr1, arr2);
-  if (eqArray) {
-    console.log(`✅✅✅ Assertion passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const flatten = function(arr) {
   let result = [];
   for (let char of arr) {
@@ -37,8 +10,5 @@ const flatten = function(arr) {
   return result;
 };
 
-// console.log(flatten([[1, 2], 4, 5]));
-// console.log(flatten([[1],2,[3, 4]]))
-// console.log(flatten([1, 2, [3, 4], 5, [6]]))
 
-assertArraysEqual(flatten[[1, 2], 3], [1, 2, 3])
+module.exports = flatten;
