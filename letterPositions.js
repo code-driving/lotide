d1c7b7c4491572a761d835854a382a0cee00b273
code-driving <tittan7@gmail.com>
2021-03-09 06:06:10 +0000

    removing custom assertions and testing with Mocha, Chai

--- letterPositions.js
+++ letterPositions.js
@@ -1,31 +1,3 @@
-// const eqArrays = require("./eqArrays");
-// const assertArraysEqual = require("./assertArraysEqual");
-
-
-const eqArrays = function(arr1, arr2) {
-  if (!Array.isArray(arr1) && !Array.isArray(arr2)) {
-    return false;
-  }
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let char of arr1) {
-    if (arr1[char] !== arr2[char]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  const eqArray = eqArrays(arr1, arr2);
-  if (eqArray) {
-    console.log(`✅✅✅ Assertion passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
@@ -39,6 +11,4 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-// console.log(letterPositions("lighthouse in the house"));
-// assertArraysEqual(letterPositions("hello").e, [1]);
-// assertArraysEqual(letterPositions("smile").s, [0]);
+module.exports = letterPositions;
