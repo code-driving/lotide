e75727d9e00a34b7839c71996f9d260b95d91a1a
code-driving <tittan7@gmail.com>
2021-03-09 04:22:34 +0000

    removing custom assertions

--- tail.js
+++ tail.js
@@ -1,5 +1,3 @@
-const assertEqual = require("./assertEqual");
-
 const tail = function(arr) {
   if (arr.length) {
     if (arr.length === 1) {
@@ -13,8 +11,3 @@ const tail = function(arr) {
 };
 
 module.exports = tail;
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.length, 2); // ensure we get back the same number (the number of the length of the array and the second argument we passed to the function)
-// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
